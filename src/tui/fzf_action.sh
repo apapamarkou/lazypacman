09e5e71c84d01676b2ffcdaa2ee6a861328cdc79
Andrianos papamarkou <papamarkoua@gmail.com>
2026-03-08 23:40:09 +0200

    Add logo

--- src/tui/fzf_action.sh
+++ src/tui/fzf_action.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+
+#  _                    ____                                  
+# | |    __ _ _____   _|  _ \ __ _  ___ _ __ ___   __ _ _ __  
+# | |   / _` |_  / | | | |_) / _` |/ __| '_ ` _ \ / _` | '_ \ 
+# | |__| (_| |/ /| |_| |  __/ (_| | (__| | | | | | (_| | | | |
+# |_____\__,_/___|\__, |_|   \__,_|\___|_| |_| |_|\__,_|_| |_|
+#                 |___/                                
+# 
+# Fast TUI + CLI package manager wrapper for Arch Linux
+# Author: Andrianos Papamarkou
+# email: papamarkoua@gmail.com
+#
 # Action helper for fzf
 
 MODULE_DIR="${MODULE_DIR:-$(dirname "$0")}"
